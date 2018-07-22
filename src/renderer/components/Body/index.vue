fc956a4764cca17d60bf0f42478780051664b4fb
CS-Tao <990296951@qq.com>
2018-07-22 23:59:22 +0800

    Change UI

--- src/renderer/components/Body/index.vue
+++ src/renderer/components/Body/index.vue
@@ -1,17 +1,73 @@
 <template>
 	<div class="body">
-
+    <el-form :model="form" ref="seatForm" label-width="50px" class="flex-row">
+      <div style="margin:auto;text-align:center;">
+        <el-form-item prop="date" label="日期">
+          <el-date-picker type="date" placeholder="请选择日期" v-model="form.date" class="input"></el-date-picker>
+        </el-form-item>
+        <el-form-item label="场馆">
+          <el-select v-model="form.library" placeholder="请选择场馆" class="input">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="房间">
+          <el-select v-model="form.room" placeholder="请选择房间" class="input">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item prop="date" label="开始">
+          <el-time-picker type="fixed-time" placeholder="请选择开始时间" v-model="form.beginTime" class="input"></el-time-picker>
+        </el-form-item>
+        <el-form-item prop="date" label="结束">
+          <el-time-picker type="fixed-time" placeholder="请选择结束时间" v-model="form.endTime" class="input"></el-time-picker>
+        </el-form-item>
+        <el-form-item prop="seatNum" label="座位">
+          <el-input-number class="num" v-model="form.seatNum" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="main-button">定时抢座</el-button>
+        </el-form-item>
+      </div>
+    </el-form>
 	</div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      form: {
+        date: '',
+        library: '',
+        room: '',
+        beginTime: '',
+        endTime: '',
+        seatNum: 1,
+        changer: false,
+        sun: false
+      }
+    }
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '@/styles/index.scss';
+.input {
+  width: 250px!important;
+  margin: 0 5px;
+}
+.num {
+  background: transparent;
+}
+.main-button {
+  margin: 15px;
+  width: 100px;
+  color: $text-color-highlight;
+  background: $primary-button-background!important;
+  border-color: $primary-color!important;
+  opacity: $button-blur-opacity;
+}
 </style>
--- src/renderer/components/Header/index.vue
+++ src/renderer/components/Header/index.vue
@@ -1,17 +1,68 @@
 <template>
 	<div class="header">
-
+    <el-form
+      :model="userInfo"
+      status-icon
+      ref="userInfo"
+      label-width="50px"
+      class="flex-row">
+      <div style="margin:auto;">
+        <el-form-item inline label="学号" prop="account" class="form-item">
+          <el-input
+            v-model.number="userInfo.account"
+            clearable
+            prefix-icon="el-icon-edit"
+            class="input">
+          </el-input>
+          <svg viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+            <path d="M5 15 L5 30 27 30 27 15 Z M9 15 C9 7 9 3 16 3 23 3 23 8 23 9 M16 20 L16 23" />
+            <circle cx="16" cy="24" r="1" />
+          </svg>
+        </el-form-item>
+        <el-form-item label="密码" prop="passwd" class="form-item">
+          <el-input type="password"
+            v-model="userInfo.passwd"
+            auto-complete="off"
+            clearable
+            prefix-icon="el-icon-edit"
+            class="input">
+          </el-input>
+          <svg viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+            <path d="M5 15 L5 30 27 30 27 15 Z M9 15 C9 9 9 5 16 5 23 5 23 9 23 15 M16 20 L16 23" />
+            <circle cx="16" cy="24" r="1" />
+          </svg>
+        </el-form-item>
+      </div>
+    </el-form>
 	</div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      userInfo: {
+        account: '',
+        passwd: ''
+      }
+    }
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '@/styles/index.scss';
+.form-item {
+  margin:10px 5px;
+  .input {
+    width: 250px;
+    margin: 0 5px;
+  }
+}
+svg {
+  display: none;
+  cursor: pointer;
+  width: 20px;
+  stroke: $second-color;
+}
 </style>
