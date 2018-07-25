29db4e6bdf3f4ffdcadeb86ae9076e7e3cf2710c
CS-Tao <990296951@qq.com>
2018-07-26 01:03:49 +0800

    Add vuex store

--- src/renderer/store/getters/index.js
+++ src/renderer/store/getters/index.js
@@ -1,15 +1,59 @@
+function formatDate (date, options) {
+  options = options || {}
+  options.sign = options.sign || 'yyyy-MM-dd'
+  var _complete = function (n) {
+    return (n > 9) ? n : '0' + n
+  }
+  var year = date.getFullYear()
+  var month = _complete(date.getMonth() + 1)
+  var day = _complete(date.getDate())
+  var result = options.sign
+  result = result.replace('yyyy', year)
+  result = result.replace('MM', month)
+  result = result.replace('dd', day)
+  return result
+}
+
+function getTomorrow () {
+  var tomorrow = new Date()
+  tomorrow.setDate(tomorrow.getDate() + 1)
+  return tomorrow
+}
+
+function getTimeInterval (beginTime = 8, count = 30, interval = 30) {
+  var timeArray = []
+  for (var i = 0; i < count; i++) {
+    timeArray.push({
+      time: beginTime * 60 + i * interval,
+      label: (beginTime + parseInt(i / 2)) + ':' + (i % 2 === 0 ? '00' : '30')
+    })
+  }
+  return timeArray
+}
+
 const getters = {
   // user info
   userAccount: state => state.app.userInfo.account,
   userPasswd: state => state.app.userInfo.passwd,
-  // setting
-  settingLibrary: state => state.app.seatInfo.library,
-  settingRoom: state => state.app.seatInfo.room,
-  settingBeginTime: state => state.app.seatInfo.beginTime,
-  settingEndTime: state => state.app.seatInfo.endTime,
-  settingSeatNum: state => state.app.seatInfo.seatNum,
-  settingBattery: state => state.app.seatInfo.battery,
-  settingSun: state => state.app.seatInfo.sun
+  userToken: state => state.app.userInfo.token,
+  hasToken: state => state.app.userInfo.token !== '0',
+  // seat info
+  seatInfoLibrary: state => state.app.seatInfo.library,
+  seatInfoRoom: state => state.app.seatInfo.room,
+  seatInfoBeginTime: state => state.app.seatInfo.beginTime,
+  seatInfoEndTime: state => state.app.seatInfo.endTime,
+  seatInfoSeatNum: state => state.app.seatInfo.seatNum,
+  seatInfoBattery: state => state.app.seatInfo.battery,
+  seatInfoSun: state => state.app.seatInfo.sun,
+  // library & room
+  freeLibrarys: state => state.app.roomInfo.buildings,
+  freeRooms: state => state.app.roomInfo.rooms,
+  // freeDates: state => state.app.roomInfo.dates,
+  freeDates: state => [formatDate(new Date()), formatDate(getTomorrow())],
+  freeBeginTime: state => getTimeInterval(),
+  freeEndTime: state => getTimeInterval(),
+  // settings
+  settingInfo: state => state.app.settingInfo
 }
 
 export default getters
--- src/renderer/store/modules/app.js
+++ src/renderer/store/modules/app.js
@@ -1,59 +1,88 @@
-import Cookies from 'js-cookie'
+import Store from 'electron-store'
 
-function getCookies (key) {
-  return Cookies.get(key) === undefined ? '' : Cookies.get(key)
+const store = new Store({
+  name: 'whu-library-seat'
+})
+
+const defaultSettingInfo = {
+  baseUrl: 'https://seat.lib.whu.edu.cn:8443',
+  oppointmentTime: new Date(2018, 4, 10, 22, 45),
+  availableTime: [new Date(2018, 4, 10, 8, 0), new Date(2018, 4, 10, 22, 30)],
+  backgroundEnable: true
 }
 
 const app = {
   state: {
     userInfo: {
-      account: getCookies('whuSeatUserAccount'),
-      passwd: getCookies('whuSeatUserPasswd')
+      account: store.get('whuSeatUserAccount', ''),
+      passwd: store.get('whuSeatUserPasswd', ''),
+      token: '0'
     },
     seatInfo: {
-      library: getCookies('whuSeatLibrary'),
-      room: getCookies('whuSeatRoom'),
-      beginTime: getCookies('whuSeatBeginTime'),
-      endTime: getCookies('whuSeatEndTime'),
-      seatNum: getCookies('whuSeatNum'),
-      battery: +Cookies.get('whuSeatBattery'),
-      sun: +Cookies.get('whuSeatSun')
+      library: store.get('whuSeatLibrary', ''),
+      room: store.get('whuSeatRoom', ''),
+      beginTime: store.get('whuSeatBeginTime', ''),
+      endTime: store.get('whuSeatEndTime', ''),
+      seatNum: store.get('whuSeatNum', 1),
+      battery: store.get('whuSeatBattery', false),
+      sun: store.get('whuSeatSun', false)
+    },
+    settingInfo: {
+      baseUrl: store.get('baseUrl', defaultSettingInfo.baseUrl),
+      oppointmentTime: new Date(store.get('oppointmentTime', defaultSettingInfo.oppointmentTime)),
+      availableTime: [new Date(store.get('availableTime', defaultSettingInfo.availableTime)[0]), new Date(store.get('availableTime', defaultSettingInfo.availableTime)[1])],
+      backgroundEnable: store.get('backgroundEnable', defaultSettingInfo.backgroundEnable)
+    },
+    roomInfo: {
+      buildings: [],
+      rooms: [],
+      dates: []
     }
   },
   mutations: {
     SET_ACCOUNT: (state, account) => {
       state.userInfo.account = account
-      Cookies.set('whuSeatUserAccount', account)
+      store.set('whuSeatUserAccount', account)
     },
     SET_PASSWD: (state, passwd) => {
       state.userInfo.passwd = passwd
-      Cookies.set('whuSeatUserPasswd', passwd)
+      store.set('whuSeatUserPasswd', passwd)
+    },
+    SET_TOKEN: (state, token) => {
+      state.userInfo.token = token
+    },
+    SAVE_SEATINFO: (state, seatInfo) => {
+      state.seatInfo.library = seatInfo.library
+      state.seatInfo.room = seatInfo.room
+      state.seatInfo.beginTime = seatInfo.beginTime
+      state.seatInfo.endTime = seatInfo.endTime
+      state.seatInfo.seatNum = seatInfo.seatNum
+      state.seatInfo.battery = seatInfo.changer
+      state.seatInfo.sun = seatInfo.sun
+      store.set('whuSeatLibrary', seatInfo.library)
+      store.set('whuSeatRoom', seatInfo.room)
+      store.set('whuSeatBeginTime', seatInfo.beginTime)
+      store.set('whuSeatEndTime', seatInfo.endTime)
+      store.set('whuSeatNum', seatInfo.seatNum)
+      store.set('whuSeatBattery', seatInfo.changer)
+      store.set('whuSeatSun', seatInfo.sun)
     },
     SAVE_SETTINGS: (state, settings) => {
-      // settings: {
-      //   library: '',
-      //   room: '',
-      //   beginTime: '',
-      //   endTime: '',
-      //   seatNum: 1,
-      //   changer: false,
-      //   sun: false
-      // }
-      state.seatInfo.library = settings.library
-      state.seatInfo.room = settings.room
-      state.seatInfo.beginTime = settings.beginTime
-      state.seatInfo.endTime = settings.endTime
-      state.seatInfo.libseatNumrary = settings.seatNum
-      state.seatInfo.battery = settings.changer
-      state.seatInfo.sun = settings.sun
-
-      Cookies.set('whuSeatLibrary', settings.library)
-      Cookies.set('whuSeatRoom', settings.room)
-      Cookies.set('whuSeatBeginTime', settings.beginTime)
-      Cookies.set('whuSeatEndTime', settings.endTime)
-      Cookies.set('whuSeatNum', settings.seatNum)
-      Cookies.set('whuSeatBattery', settings.changer ? 1 : 0)
-      Cookies.set('whuSeatSun', settings.sun ? 1 : 0)
+      state.settingInfo = settings
+      store.set('baseUrl', settings.baseUrl)
+      store.set('oppointmentTime', settings.oppointmentTime)
+      store.set('availableTime', settings.availableTime)
+      store.set('backgroundEnable', settings.backgroundEnable)
+    },
+    RESTORE_SETTINGS: (state) => {
+      state.settingInfo = defaultSettingInfo
+      store.set('baseUrl', defaultSettingInfo.baseUrl)
+      store.set('oppointmentTime', defaultSettingInfo.oppointmentTime)
+      store.set('availableTime', defaultSettingInfo.availableTime)
+      store.set('backgroundEnable', defaultSettingInfo.backgroundEnable)
+    },
+    SAVE_ROOM_INFO: (state, data) => {
+      state.roomInfo = data
     }
   },
   actions: {
@@ -63,8 +92,20 @@ const app = {
     setPasswd ({ commit }, passwd) {
       commit('SET_PASSWD', passwd)
     },
+    setToken ({ commit }, token) {
+      commit('SET_TOKEN', token)
+    },
+    saveSeatInfo ({ commit }, seatInfo) {
+      commit('SAVE_SEATINFO', seatInfo)
+    },
     saveSettings ({ commit }, settings) {
       commit('SAVE_SETTINGS', settings)
+    },
+    restoreSettings ({ commit }) {
+      commit('RESTORE_SETTINGS')
+    },
+    saveRoomInfo ({ commit }, data) {
+      commit('SAVE_ROOM_INFO', data)
     }
   }
 }
