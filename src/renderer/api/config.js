211c22d40f06c67f8a3693d1bd6da7a5719b70d3
CS-Tao <990296951@qq.com>
2018-07-26 01:04:17 +0800

    Add apis

---
+++ src/renderer/api/config.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import Store from 'electron-store'
+import { Notification } from 'element-ui'
+
+const store = new Store({
+  name: 'whu-library-seat'
+})
+
+// create an axios instance
+const service = axios.create({
+  baseURL: store.get('baseUrl', 'https://seat.lib.whu.edu.cn:8443'),
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+  },
+  withCredentials: true
+})
+
+// request interceptor
+service.interceptors.request.use(config => {
+  // Do something before request is sent
+  return config
+}, error => {
+  // Do something with request error
+  Promise.reject(error)
+})
+
+// respone interceptor
+service.interceptors.response.use(
+  response => response,
+  error => {
+    Notification({
+      message: error.message,
+      type: 'error',
+      duration: 3 * 1000
+    })
+    return Promise.reject(error)
+  })
+
+export default service
