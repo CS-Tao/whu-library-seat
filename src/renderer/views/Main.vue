ae35137d376e7fcfb7e3865812100b4958c9a912
CS-Tao <990296951@qq.com>
2018-07-26 01:03:08 +0800

    Change display method

--- src/renderer/views/Main.vue
+++ src/renderer/views/Main.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="main">
     <main-header></main-header>
-    <main-body></main-body>
-    <main-footer></main-footer>
+    <transition name="el-fade-in-linear">
+      <main-body v-if="hasToken" :showMode="bodyMode"></main-body>
+    </transition>
+    <main-footer :bodyMode="bodyMode" @iconClicked="footerIconClicked($event)"></main-footer>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import MainHeader from '@/components/Header'
 import MainFooter from '@/components/Footer'
 import MainBody from '@/components/Body'
+
 export default {
+  data () {
+    return {
+      bodyMode: 'normal'
+    }
+  },
   components: {
     MainHeader, MainBody, MainFooter
   },
-  data () {
-    return {}
+  computed: {
+    ...mapGetters([
+      'hasToken'
+    ])
+  },
+  methods: {
+    footerIconClicked (param) {
+      if (param === 'user') {
+        this.bodyMode = 'userForm'
+      } else if (param === 'history') {
+        this.bodyMode = 'historyForm'
+      } else if (param === 'normal') {
+        this.bodyMode = 'normal'
+      } else {
+        console.log('参数错误')
+      }
+    }
   }
 }
 </script>
