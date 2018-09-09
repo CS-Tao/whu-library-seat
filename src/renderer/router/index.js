2607f3ee260f5ab67f8ebf87a5dc4e211086120b
CS-Tao <990296951@qq.com>
2018-09-10 00:46:29 +0800

    Move main page location

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'landing-page',
-      component: require('@/views/Main').default
+      component: require('@/components/Main').default
     },
     {
       path: '*',
