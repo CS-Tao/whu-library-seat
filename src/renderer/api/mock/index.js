19cd3572dd08ee332646e238f9541cc28932d102
CS-Tao <990296951@qq.com>
2018-10-05 00:56:09 +0800

    Fix mock

--- src/renderer/api/mock/index.js
+++ src/renderer/api/mock/index.js
@@ -5,43 +5,43 @@ import libraryApi from './library.api'
 import urls from '../urls'
 
 // 用户验证
-Mock.mock(urls.gitcontents.validateUser.regular, gitcontentsApi.validateUser.method, gitcontentsApi.validateUser)
+Mock.mock(urls.gitcontents.validateUser.regular, urls.gitcontents.validateUser.method, gitcontentsApi.validateUser)
 
 // 登录状态
-Mock.mock(urls.usage.loginState.regular, usageApi.loginState.method, usageApi.loginState)
+Mock.mock(urls.usage.loginState.regular, urls.usage.loginState.method, usageApi.loginState)
 
 // 抢座状态
-Mock.mock(urls.gitcontents.grabState.regular, usageApi.grabState.method, usageApi.grabState)
+Mock.mock(urls.usage.grabState.regular, urls.usage.grabState.method, usageApi.grabState)
 
 // 登录
-Mock.mock(urls.library.Login.regular, libraryApi.Login.method, libraryApi.Login)
+Mock.mock(urls.library.Login.regular, urls.library.Login.method, libraryApi.Login)
 
 // 得到可以使用的房间信息
-Mock.mock(urls.library.FreeFilters.regular, libraryApi.FreeFilters.method, libraryApi.FreeFilters)
+Mock.mock(urls.library.FreeFilters.regular, urls.library.FreeFilters.method, libraryApi.FreeFilters)
 
 // 得到房间详细信息
-Mock.mock(urls.library.RoomStats.regular, libraryApi.RoomStats.method, libraryApi.RoomStats)
+Mock.mock(urls.library.RoomStats.regular, urls.library.RoomStats.method, libraryApi.RoomStats)
 
 // 验证 token 是否可用
-Mock.mock(urls.library.ValidateToken.regular, libraryApi.ValidateToken.method, libraryApi.ValidateToken)
+Mock.mock(urls.library.ValidateToken.regular, urls.library.ValidateToken.method, libraryApi.ValidateToken)
 
 // 得到位置详细信息
-Mock.mock(urls.library.LayoutByDate.regular, libraryApi.LayoutByDate.method, libraryApi.LayoutByDate)
+Mock.mock(urls.library.LayoutByDate.regular, urls.library.LayoutByDate.method, libraryApi.LayoutByDate)
 
 // 按时间搜索位置
-Mock.mock(urls.library.SearchSeat.regular, libraryApi.SearchSeat.method, libraryApi.SearchSeat)
+Mock.mock(urls.library.SearchSeat.regular, urls.library.SearchSeat.method, libraryApi.SearchSeat)
 
 // 预约位置
-Mock.mock(urls.library.Book.regular, libraryApi.Book.method, libraryApi.Book)
+Mock.mock(urls.library.Book.regular, urls.library.Book.method, libraryApi.Book)
 
 // 取消预约
-Mock.mock(urls.library.Cancel.regular, libraryApi.Cancel.method, libraryApi.Cancel)
+Mock.mock(urls.library.Cancel.regular, urls.library.Cancel.method, libraryApi.Cancel)
 
 // 得到用户信息
-Mock.mock(urls.library.User.regular, libraryApi.User.method, libraryApi.User)
+Mock.mock(urls.library.User.regular, urls.library.User.method, libraryApi.User)
 
 // 得到预约历史
-Mock.mock(urls.library.History.regular, libraryApi.History.method, libraryApi.History)
+Mock.mock(urls.library.History.regular, urls.library.History.method, libraryApi.History)
 
 // 终止使用
-Mock.mock(urls.library.Stop.regular, libraryApi.Stop.method, libraryApi.Stop)
+Mock.mock(urls.library.Stop.regular, urls.library.Stop.method, libraryApi.Stop)
