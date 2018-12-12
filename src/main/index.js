a40ea2b6f30757ce4733cf3cb64065bc716c0576
CS-Tao <990296951@qq.com>
2018-12-13 00:09:05 +0800

    Delete '权限' menu

--- src/main/index.js
+++ src/main/index.js
@@ -138,18 +138,6 @@ const template = [
       }
     ]
   }, {
-    label: '权限',
-    submenu: [
-      {
-        label: '申请权限',
-        click () { require('electron').shell.openExternal('https://home.cs-tao.cc/whu-library-seat/specification/#申请软件使用权') }
-      },
-      {
-        label: '查看白名单',
-        click () { require('electron').shell.openExternal('https://github.com/CS-Tao/whu-library-seat/blob/user-validation/validation.json') }
-      }
-    ]
-  }, {
     label: '关于',
     role: 'about',
     submenu: [
