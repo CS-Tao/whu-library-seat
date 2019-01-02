577bcd02f0c3d14821210aea53e3488911118dea
CS-Tao <990296951@qq.com>
2019-01-02 23:21:56 +0800

    Show 2017 data

--- routes/yearData.js
+++ routes/yearData.js
@@ -3,24 +3,24 @@ var mysql = require("mysql");
 var mysqlConfig = require("./mysql.conf");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   var connection = mysql.createConnection(mysqlConfig.config);
   var now = new Date()
 
-  var queryString = `SELECT year, month, day, count(*) as 'count' FROM events where (code = 6 && (year=${now.getFullYear()}) || (year=${now.getFullYear() - 1} && month <= ${now.getMonth() + 1}) || (year=${now.getFullYear() - 1} && month=${now.getMonth() + 1} && day <= ${now.getDate()})) group by year, month, day;`;
+  var queryString = `SELECT year, month, day, count(*) as 'count' FROM events where (code = 6 && (year=${now.getFullYear()} || year=${now.getFullYear() - 1}) || (year=${now.getFullYear() - 1} && month <= ${now.getMonth() + 1}) || (year=${now.getFullYear() - 1} && month=${now.getMonth() + 1} && day <= ${now.getDate()})) group by year, month, day;`;
 
   connection.query(queryString, (err, results) => {
-      if (err) {
-        console.log(err.message)
-        res.json([]);
-        return;
-      } else {
-        let data = Object()
-        results.forEach(element => {
-          data[new Date(element.year, element.month - 1, element.day).getTime().toString().substring(0, 10)] = element.count
-        });
-        res.json(data);
-      }
+    if (err) {
+      console.log(err.message)
+      res.json([]);
+      return;
+    } else {
+      let data = Object()
+      results.forEach(element => {
+        data[new Date(element.year, element.month - 1, element.day).getTime().toString().substring(0, 10)] = element.count
+      });
+      res.json(data);
+    }
   });
   connection.end();
 });
