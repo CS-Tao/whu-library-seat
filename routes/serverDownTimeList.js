eb34b8ea85f835c968ec03c1c95bff6ffb01a94f
CS-Tao <990296951@qq.com>
2020-01-06 21:13:52 +0800

    Fix

--- routes/serverDownTimeList.js
+++ routes/serverDownTimeList.js
@@ -1,7 +1,7 @@
 const serverDownTimeList = [
   {
     begin: new Date(2019, 5 - 1, 21).getTime(),
-    end: new Date(2020, 5 - 1, 25).getTime()
+    end: new Date(2019, 5 - 1, 25).getTime()
   },
   {
     begin: new Date(2019, 7 - 1, 6).getTime(),
